99bd85a7e398f9a88aeef600680290734bd04b68
Esteban Reveles Valenzuela <reveles.esteban@uabc.edu.mx>
2026-06-26 22:07:40 -0700

    Actualización de logo de Vanko's Throne. Se ajustó el volumen de alguans canciones. Se movió la barra de XP para no tapar su label al subir de nivel. Icono con mejor resolución.

--- Scripts/start_screen.gd
+++ Scripts/start_screen.gd
@@ -1,9 +1,10 @@
 extends Control
 
 @onready var sunburn = $SunBurn
-@onready var vankos = $Vanks
-@onready var timer = $Timer
-var showed = false
+@onready var vankos = $Vankos
+@onready var quit_timer = $QuitTimer
+var can_disappear = false
+
 func _ready():
 	sunburn.modulate.a = 0.0
 	vankos.modulate.a = 0.0
@@ -11,18 +12,21 @@ func _ready():
 
 func _process(delta: float) -> void:
 	
-	if(sunburn.modulate.a < 1.0 and not showed):
-		sunburn.modulate.a += 0.4*delta
-		vankos.modulate.a += 0.4*delta
-		if(sunburn.modulate.a >= 1.0): showed = true
-	elif (sunburn.modulate.a > 0.0 and showed):
-		sunburn.modulate.a -= 0.4*delta
-		vankos.modulate.a -= 0.4*delta
+	if sunburn.modulate.a < 1.0 and !can_disappear:
+		sunburn.modulate.a += 0.75*delta
+		vankos.modulate.a += 0.75*delta
+		
+	if can_disappear:
+		sunburn.modulate.a -= 0.75*delta
+		vankos.modulate.a -= 0.75*delta
 
 func _input(event: InputEvent):
 	if(event.is_action_pressed("ui_accept")):
-		timer.stop()
+		quit_timer.stop()
 		get_tree().change_scene_to_file("res://Scenes/Scenario/main_menu.tscn")
+		
+func _on_disappear_timer_timeout() -> void:
+	can_disappear = true
 
 func _on_timer_timeout() -> void:
 	MusicManager.resumeMusic()
